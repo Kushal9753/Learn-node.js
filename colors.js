f712cd46b66c0d18de9aa6102246903ac25db757
Kushal9753 <kushalsahu352@gmail.com>
2025-11-02 14:13:50 +0530

    add colors.js to demonstrate colored console output

---
+++ colors.js
@@ -0,0 +1,8 @@
+const colors = require('colors')
+
+console.log(colors.bgGreen("Hi this is kushal sahu"));
+console.log(colors.bgRed("Hi this is kushal sahu"));
+console.log(colors.bgYellow("Hi this is kushal sahu"));
+console.log(colors.green("Hi this is kushal sahu"));
+console.log(colors.red("Hi this is kushal sahu"));
+console.log(colors.yellow("Hi this is kushal sahu"));
