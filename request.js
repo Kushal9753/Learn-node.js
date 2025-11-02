d0f795788dd620ac37fce0e503a3038f98235219
Kushal9753 <kushalsahu352@gmail.com>
2025-11-02 16:27:26 +0530

    add request.js to implement a basic HTTP server with routing

---
+++ request.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+http.createServer((req,resp)=>{
+  console.log(req.method);
+  if(req.url=="/"){
+  resp.write("<h1>Home Page</h1>")
+
+  }
+  else if(req.url=="/login"){
+      resp.write("<h1>Login Page</h1>")
+
+
+  }else{
+    resp.write("other page")
+
+  }
+  
+  resp.end()
+}).listen(5600)
